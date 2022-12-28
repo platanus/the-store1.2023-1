e7a17190aa6e1450b283e5204dbfd223aa186a67
galvadiz <geraldine.alvadiz@ug.uchile.cl>
2022-12-28 11:23:02 -0300

    feat(items-controller): add show route

--- app/controllers/app/items_controller.rb
+++ app/controllers/app/items_controller.rb
@@ -2,4 +2,18 @@ class App::ItemsController < App::BaseController
   def index
     @items = Item.all
   end
+
+  def show
+    item
+  end
+
+  private
+
+  def item
+    @item ||= Item.find(item_params[:id])
+  end
+
+  def item_params
+    params.permit(:id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
   scope module: :app, defaults: { format: :html } do
     resources :purchases, only: [:index]
+    resources :items, only: [:index, :show]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
