366968d3136ff97a1ea8adceaa6fb9f1ec1cbfdc
galvadiz <geraldine.alvadiz@ug.uchile.cl>
2022-12-29 14:51:24 -0300

    chore(vue-inline-svg): add vue-inline-svg

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import Notifications from '@kyvg/vue3-notification';
+import InlineSvg from 'vue-inline-svg';
 import TheHeader from './components/the-header.vue';
 import BaseNotifications from './components/base-notifications.vue';
 import BaseInput from './components/base-input.vue';
@@ -25,6 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
   app.component('BaseInput', BaseInput);
   app.component('BaseButton', BaseButton);
   app.component('BaseCheckbox', BaseCheckbox);
+  app.component('InlineSvg', InlineSvg);
   app.use(Notifications);
   app.mount('#vue-app');
 
