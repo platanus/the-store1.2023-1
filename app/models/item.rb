54a0b1806eaf8b62a78151191514e20999dd314d
Leandro Segovia <ldlsegovia@gmail.com>
2023-01-04 16:48:26 -0300

    tests

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,28 @@
 class Item < ApplicationRecord
   include ImageUploader::Attachment(:image)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   validates :name, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
 end
