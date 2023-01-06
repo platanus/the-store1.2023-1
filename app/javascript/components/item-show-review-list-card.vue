bf4b7d5386c3a11ec31a6407ccc9f4e45d5b7c24
galvadiz <geraldine.alvadiz@ug.uchile.cl>
2023-01-06 10:41:26 -0300

    feat(item-show-review): add views to review list

---
+++ app/javascript/components/item-show-review-list-card.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import formatDate from '../utils/format_date.ts';
+
+type Props = {
+  userEmail: string
+  body: string
+  createdAt: string
+};
+defineProps<Props>();
+</script>
+
+<template>
+  <div class="flex flex-col gap-4">
+    <div class="flex w-full items-center justify-end">
+      <span class="text-sm text-zinc-500"> {{ formatDate(createdAt) }} </span>
+    </div>
+    <span
+      v-if="body"
+      class="text-base text-zinc-800"
+    >
+      {{ body }}
+    </span>
+    <span
+      v-else
+      class="text-base italic text-zinc-300"
+    >
+      No dejó comentario
+    </span>
+    <span class="text-base font-bold text-zinc-800"> {{ userEmail }} </span>
+    <div class="py-4 px-12">
+      <hr class="border-t border-zinc-800">
+    </div>
+  </div>
+</template>
---
+++ app/javascript/components/item-show-review.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { Review } from '../api/reviews';
+import ItemShowReviewList from './item-show-review-list.vue';
+
+type Props = {
+  reviews: Review[]
+};
+defineProps<Props>();
+
+</script>
+
+<template>
+  <div class="flex flex-col content-start gap-8">
+    <span class="text-xl font-bold text-zinc-800">
+      Opiniones del producto
+    </span>
+    <hr class="border-t border-zinc-800">
+    <item-show-review-list
+      :reviews="reviews"
+    />
+  </div>
+</template>
