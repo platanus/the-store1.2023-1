b65f64e120966c49983ebc3bf66ca23836ca4053
galvadiz <geraldine.alvadiz@ug.uchile.cl>
2023-01-06 18:51:39 -0300

    feat(item-show-review-form): add form write review

---
+++ app/javascript/components/item-show-review-write.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { Item } from '../api/items';
+
+type Props = {
+  item: Item
+};
+defineProps<Props>();
+
+defineEmits<{(e: 'openReviewForm'): void}>();
+</script>
+<template>
+  <div class="flex w-full flex-col items-center">
+    <p class="text-base text-zinc-800">
+      Cómo te fue con el producto?
+    </p>
+    <base-button
+      class="w-full border"
+      variant="secondary"
+      @click="$emit('openReviewForm')"
+    >
+      Escribir mi opinión
+    </base-button>
+  </div>
+</template>
--- app/javascript/components/item-show-review.vue
+++ app/javascript/components/item-show-review.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import type { Item } from '../api/items';
 import type { Review } from '../api/reviews';
+import ItemShowReviewWrite from './item-show-review-write.vue';
+import ItemShowReviewForm from './item-show-review-form.vue';
 import ItemShowReviewList from './item-show-review-list.vue';
 
 type Props = {
+  item: Item
   reviews: Review[]
 };
 defineProps<Props>();
 
+const showReviewForm = ref(false);
 </script>
 
 <template>
@@ -14,7 +20,19 @@ defineProps<Props>();
     <span class="text-xl font-bold text-zinc-800">
       Opiniones del producto
     </span>
+    <div class="flex w-full gap-16">
+      <item-show-review-write
+        v-if="!showReviewForm"
+        :item="item"
+        @open-review-form="showReviewForm = true"
+      />
+    </div>
     <hr class="border-t border-zinc-800">
+    <item-show-review-form
+      v-if="showReviewForm"
+      :item="item"
+      @close-review-form="showReviewForm = false"
+    />
     <item-show-review-list
       :reviews="reviews"
     />
